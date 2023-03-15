1366e14ac35e0fa8cbde536228889f8234ec697f
Yuliia Andrieieva <118940236+andrieieva-yuliia@users.noreply.github.com>
2023-03-16 01:05:36 +0200

    Header completed (#6)
    * Header completed
    
    * Logo fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import { Catalog } from './components/Catalog';
 import { Footer } from './components/Footer';
 import { PhonesPage } from './pages/PhonesPage';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { Header } from './components/Header';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header />
+
       <Routes>
         <Route path="/" element={<Catalog />} />
         <Route path="home" element={<Navigate to="/" replace />} />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.nav__logo}>
+          <a href="#home">
+            <img
+              className={styles.nav__logo__image}
+              src="/img/icons/logo.svg"
+              alt="logo"
+            />
+          </a>
+        </div>
+
+        <ul className={styles.nav__list}>
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link__active} href="#apple">Home</a>
+          </li>
+
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link} href="#phones">Phones</a>
+          </li>
+
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link} href="#tablets">Tablets</a>
+          </li>
+
+          <li className={styles.nav__item}>
+            <a
+              className={styles.nav__link}
+              href="#accessories"
+            >
+              Accessories
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.icon}>
+        <a className={styles.icon_favourites} href="#favourites">
+        </a>
+
+        <a className={styles.icon_shop_bag} href="#shopBag">
+        </a>
+
+        <a className={styles.icon_burger_menu} href="#burgerMenu">
+        </a>
+      </div>
+    </header>
+  );
+};
