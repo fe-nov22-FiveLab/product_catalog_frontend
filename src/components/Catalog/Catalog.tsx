e073015fa6511f60c3b2fe26433c429d00656fa2
victorionov <victorionovcvlrs@gmail.com>
2023-03-15 18:14:20 +0100

    finished card

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styles from './Catalog.module.scss';
+import { Card } from '../Card/Card';
 
 export const Catalog: React.FC = () => {
-  return <div className={styles.container}>Catalog</div>;
+  return(
+    <>
+      <div className={styles.container}>Catalog
+        <Card />
+      </div>
+
+    </>
+  );
 };
