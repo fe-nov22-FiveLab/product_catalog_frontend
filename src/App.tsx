6471c83ad737989ac6963f9a69e30524fc34a81d
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2023-03-15 17:23:33 +0200

    add routing for notFoundPage and PhonesPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Catalog } from './components/Catalog';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { PhonesPage } from './pages/PhonesPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 const App: React.FC = () => {
   return (
@@ -8,6 +10,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Catalog />} />
         <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="/phones" element={<PhonesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PhonesPage: React.FC = () => {
+  return <h1 className="">Mobile phones</h1>;
+};
