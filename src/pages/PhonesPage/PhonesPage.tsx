0725970761ddbd8fcb6bc8d050ba5100e27e0e92
Anastasiia-Khudych <101983641+Anastasiia-Khudych@users.noreply.github.com>
2023-03-16 23:10:36 +0200

    add dropdowns for phone catalog (#13)

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './PhonesPage.module.scss';
 import { Catalog } from '../../components/Catalog';
+import { PhonesFilter } from '../../components/PhonesFilter';
 
 export const PhonesPage: React.FC = () => {
   return (
     <>
       <h1 className={styles.title}>Mobile phones</h1>
+      <PhonesFilter />
       <Catalog />
     </>
   );
