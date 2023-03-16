9758efe0e8f37c7b88823b631fb75013444fb2da
Yuliia Andrieieva <118940236+andrieieva-yuliia@users.noreply.github.com>
2023-03-16 22:04:05 +0200

    Burger menu completed (#11)
    * Header completed
    
    * Logo fixed
    
    * Header routing added
    
    * routes fixes
    
    * Burger menu completed

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,5 +1,92 @@
 import React from 'react';
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+import styles from './BurgerMenu.module.scss';
+import logo from '../../assets/img/logo.svg';
+import cross from '../../assets/img/cross.svg';
+import favourites_heart from '../../assets/img/favourites_heart.svg';
+import bag from '../../assets/img/bag.svg';
 
-export const BurgerMenu: React.FC = () => {
-  return <></>;
+type Props = {
+  handleCloseBurger: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const BurgerMenu: React.FC<Props> = ({ handleCloseBurger }) => {
+  return (
+    <nav className={styles.burger}>
+      <div className={styles.burger__container}>
+        <div className={styles.burger__logo}>
+          <NavLink to="home">
+            <img className={styles.burger__logo__image} src={logo} />
+          </NavLink>
+        </div>
+        <button
+          onClick={() => handleCloseBurger(false)}
+          type="button"
+          className={styles.burger__button}
+        >
+          <img src={cross} />
+        </button>
+      </div>
+
+      <div className={styles.burger__list}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => classNames(styles.burger__link, { [styles.is_active]: isActive })}
+        >
+          HOME
+        </NavLink>
+
+        <NavLink
+          to="phones"
+          className={({ isActive }) => classNames(styles.burger__link, { [styles.is_active]: isActive })}
+        >
+          PHONES
+        </NavLink>
+
+        <NavLink
+          to="tablets"
+          className={({ isActive }) => classNames(styles.burger__link, { [styles.is_active]: isActive })}
+        >
+          TABLETS
+        </NavLink>
+
+        <NavLink
+          to="accessories"
+          className={({ isActive }) => classNames(styles.burger__link, { [styles.is_active]: isActive })}
+        >
+          ACCESSORIES
+        </NavLink>
+      </div>
+
+      <div className={styles.burger__container__bottom}>
+        <div className={styles.icon}>
+          <div className={styles.icon__action__favourites}>
+            <NavLink
+              to="favourites"
+              className={({ isActive }) => classNames(styles.icon__action__favourites, {
+                [styles.is_active]: isActive,
+              })}
+
+            >
+              <img src={favourites_heart} />
+            </NavLink>
+          </div>
+        </div>
+
+        <div className={styles.icon}>
+          <div className={styles.icon__action}>
+            <NavLink
+              to="cart"
+              className={({ isActive }) => classNames(styles.icon__action__shop_bag, {[
+              styles.is_active]: isActive,
+              })}
+            >
+              <img src={bag} />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Nav } from '../Nav/Nav';
 import styles from './Header.module.scss';
 import { BurgerMenu } from '../BurgerMenu/BurgerMenu';
+import favourites_heart from '../../assets/img/favourites_heart.svg';
+import bag from '../../assets/img/bag.svg';
+import burger from '../../assets/img/burger.svg';
 
 export const Header: React.FC = () => {
+  const [isActiveBurger, setIsActiveBurger] = useState(false);
+
   return (
     <>
+      {isActiveBurger && (
+        <BurgerMenu
+          handleCloseBurger={setIsActiveBurger}
+        />
+      )}
       <header className={styles.header}>
         <Nav />
 
         <div className={styles.icon}>
           <NavLink to="favourites">
             <div className={styles.icon__action}>
-              <img src="img/icons/favourites.svg" alt="favourites" />
+              <img src={favourites_heart} alt="favourites" />
             </div>
           </NavLink>
 
           <NavLink to="cart">
             <div className={styles.icon__action}>
-              <img src="img/icons/bag.svg" alt="shopping bag" />
+
+              <img src={bag} />
             </div>
           </NavLink>
 
-          <BurgerMenu />
+          <button
+            onClick={() => setIsActiveBurger(true)}
+            type="button"
+            className={styles.icon__action__burger_menu}
+          >
+            <img src={burger} />
+          </button>
         </div>
       </header>
     </>
