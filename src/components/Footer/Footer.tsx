e6bc45fabe3c33b7f5cc1255123efa577aade0fa
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2023-03-16 22:12:41 +0200

    fixed flex main content, added  button to top

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,42 +3,60 @@ import styles from './Footer.module.scss';
 // import logo from '../../../public/img/icons/logo.svg';
 import { Link } from 'react-router-dom';
 
-export const Footer: React.FC = () => (
-  <div className={styles.container}>
-    <div className={styles.content}>
-      <footer className={styles.footer}>
-        <img
-          className={styles.footer_logo}
-          src="img/icons/logo.svg"
-          alt="logo nice gadgets"
-        />
+export const Footer: React.FC = () => {
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-        <ul className={styles.footer_list}>
-          <li className={styles.footer_item}>
-            <Link to={'/github'} className={styles.footer_link}>
-              github
-            </Link>
-          </li>
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <footer className={styles.footer}>
+          <img
+            className={styles.footer_logo}
+            src="img/icons/logo.svg"
+            alt="logo nice gadgets"
+          />
 
-          <li className={styles.footer_item}>
-            <Link to={'/contacts'} className={styles.footer_link}>
-              contacts
-            </Link>
-          </li>
+          <ul className={styles.footer_list}>
+            <li className={styles.footer_item}>
+              <Link to={'/github'} className={styles.footer_link}>
+                github
+              </Link>
+            </li>
 
-          <li className={styles.footer_item}>
-            <Link to={'/rights'} className={styles.footer_link}>
-              rights
-            </Link>
-          </li>
-        </ul>
+            <li className={styles.footer_item}>
+              <Link to={'/contacts'} className={styles.footer_link}>
+                contacts
+              </Link>
+            </li>
 
-        <div className={styles.footer_top_button}>
-          <p className={styles.footer_top_title}>Back to top</p>
+            <li className={styles.footer_item}>
+              <Link to={'/rights'} className={styles.footer_link}>
+                rights
+              </Link>
+            </li>
+          </ul>
 
-          <Link to={'#'} className={styles.footer_arrow_up}></Link>
-        </div>
-      </footer>
+          <div className={styles.footer_top_button}>
+            <label
+              className={styles.footer_top_title}
+              htmlFor="back-to-top"
+            >
+              Back to top
+            </label>
+
+            <button type='button'
+              className={styles.footer_arrow_up}
+              id="back-to-top"
+              onClick={goToTop}></button>
+          </div>
+        </footer>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
