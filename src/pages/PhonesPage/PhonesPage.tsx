86d5477ff433f794edb83f0d62679f579f30ee01
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2023-03-17 20:19:28 +0200

    add pagination with searchParams

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import styles from './PhonesPage.module.scss';
 import { Catalog } from '../../components/Catalog';
 import { PhonesFilter } from '../../components/PhonesFilter';
+import { Pagination } from '../../components/Pagination';
 
 export const PhonesPage: React.FC = () => {
+  // const total = 95;
+  // get data from server here: phones, total
   return (
     <>
       <h1 className={styles.title}>Mobile phones</h1>
       <PhonesFilter />
       <Catalog />
+      <Pagination total={95} />
     </>
   );
 };
+// total={95}
