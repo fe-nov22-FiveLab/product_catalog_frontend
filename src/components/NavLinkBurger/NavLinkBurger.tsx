cef7f0381222bdcffe97d84d2f3356b5454245a4
Yuliia Andrieieva <118940236+andrieieva-yuliia@users.noreply.github.com>
2023-03-17 17:48:38 +0200

    Burger fixes (#15)
    * Header completed
    
    * Logo fixed
    
    * Header routing added
    
    * routes fixes
    
    * Burger menu completed
    
    * Burger fixed, breadcrumbs added, NotFoundPage added
    
    * burges fixes
    
    * burger fixes
    
    * burger fixes

--- src/components/NavLinkBurger/NavLinkBurger.tsx
+++ src/components/NavLinkBurger/NavLinkBurger.tsx
@@ -6,16 +6,19 @@ import styles from '../BurgerMenu/BurgerMenu.module.scss';
 interface Props {
   path: string;
   text: string;
+  isOpenMenu?: (isOpen: boolean) => void;
 }
 
 export const NavLinkBurger: React.FC<Props> = ({
   path,
   text,
+  isOpenMenu,
 }) => {
   return (
     <NavLink
       to={path}
       className={({ isActive }) => classNames(styles.burger__link, { [styles.is_active]: isActive })}
+      onClick={() => isOpenMenu !== undefined && isOpenMenu(false)}
     >
       {text}
     </NavLink>
