fd2359612b4db26d47e2caf9f5ffff1c0598639a
Yuliia Andrieieva <118940236+andrieieva-yuliia@users.noreply.github.com>
2023-03-17 14:04:16 +0200

    Added not found page and breadcrumbs, burger fixed (#14)
    * Header completed
    
    * Logo fixed
    
    * Header routing added
    
    * routes fixes
    
    * Burger menu completed
    
    * Burger fixed, breadcrumbs added, NotFoundPage added

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './PhonesPage.module.scss';
 import { Catalog } from '../../components/Catalog';
 import { PhonesFilter } from '../../components/PhonesFilter';
+import { Breadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs';
 
 export const PhonesPage: React.FC = () => {
   return (
     <>
+      <Breadcrumbs location="phones" />
       <h1 className={styles.title}>Mobile phones</h1>
       <PhonesFilter />
       <Catalog />
