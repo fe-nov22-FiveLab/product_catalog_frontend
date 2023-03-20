6431902e8fe89fbd573310c3d63a28d2a56dd6e1
victorionov <victorionovcvlrs@gmail.com>
2023-03-20 16:10:51 +0100

    cart CheckoutModal

--- src/components/CartCheckout/CartCheckout.tsx
+++ src/components/CartCheckout/CartCheckout.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+
+import { useState } from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { selectCart } from '../../features/cart/cartSlice';
+import { CheckoutModal } from '../CheckoutModal/CheckoutModal';
 import styles from './CartCheckout.module.scss';
 
 export const CartCheckout: React.FC = () => {
   const { phones, totalPrice } = useAppSelector(selectCart);
+  const [isShown, setIsShown] = useState<boolean>(false);
+
+  const handleModal = () => {
+    if (phones.length) {
+      setIsShown(true);
+    }
+  };
 
   const totalPhones = phones.reduce(
     (sum, phone) => (phone.count as number) + sum,
@@ -21,11 +31,22 @@ export const CartCheckout: React.FC = () => {
             `}
           </p>
           <div className={styles.line}></div>
-          <button className={styles.button_checkout} data-qa="hover">
+
+          <button
+            className={styles.button_checkout} data-qa="hover"
+            onClick={handleModal}
+          >
             Checkout
           </button>
         </div>
       </div>
+
+      {isShown && (
+        <CheckoutModal
+          isShown={isShown}
+          setIsShown={setIsShown}/>
+      )}
+
     </>
   );
 };
