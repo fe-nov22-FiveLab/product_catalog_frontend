d3d60c780ba974a20d0b5b33632364c149dd9d04
Yuliia Andrieieva <118940236+andrieieva-yuliia@users.noreply.github.com>
2023-03-21 19:19:09 +0200

    Page not found (#37)
    * Header completed
    
    * Logo fixed
    
    * Header routing added
    
    * routes fixes
    
    * Burger menu completed
    
    * Burger fixed, breadcrumbs added, NotFoundPage added
    
    * burges fixes
    
    * burger fixes
    
    * burger fixes
    
    * saved
    
    * Fixed linter issues
    
    * commit before pull
    
    * itemCard without buttons
    
    * pull
    
    * fixes
    
    * Page not found

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { CowGame } from '@gnosis.pm/cow-runner-game';
+import { Link } from 'react-router-dom';
 import styles from './NotFoundPage.module.scss';
 
-export const NotFoundPage: React.FC = () => (
-  <div className={styles.container}>
-    <h1>404 Page not found</h1>
-    <br></br>
-    <p>Let&apos;s play cow game</p>
-    <p>Try not to get 🥪</p>
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>😔 404 Page not found</h1>
+      <br></br>
+      <p className={styles.text}>Let&apos;s play cow game</p>
+      <p className={styles.text}>Try not to get 🥪</p>
+      <br></br>
 
-    <CowGame />
-  </div>
-);
+      <CowGame />
+
+      <div className={styles.button_container}>
+        <Link to='/'>
+          <button className={styles.button_back}>Back to home</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
