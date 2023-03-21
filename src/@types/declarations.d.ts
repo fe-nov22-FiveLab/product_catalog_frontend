0ce509e7d9e3e17f2e35352e2e0dd1905058701f
Anastasiia-Khudych <101983641+Anastasiia-Khudych@users.noreply.github.com>
2023-03-21 20:54:22 +0200

    add search for phones (#38)

--- src/@types/declarations.d.ts
+++ src/@types/declarations.d.ts
@@ -3,3 +3,4 @@ declare module '*.svg';
 declare module '*.png';
 declare module '*.jpg';
 declare module 'redux-persist/lib/storage';
+declare module 'lodash.debounce';
---
+++ src/components/PhonesSearch/PhonesSearch.tsx
@@ -0,0 +1,42 @@
+import React, { ChangeEvent, useCallback, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { getSearchWith } from '../../utils/searchHelper';
+import styles from './PhonesSearch.module.scss';
+import debounce from 'lodash.debounce';
+
+export const PhonesSearch: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState('');
+
+  const debouncedSetSearchParams = useCallback(
+    debounce(setSearchParams, 1000),
+    [],
+  );
+
+  const onQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setQuery(value);
+
+    const normalizedValue = value.trim();
+
+    const newSearchParams = getSearchWith(searchParams, {
+      query: normalizedValue || null,
+    });
+
+    debouncedSetSearchParams(newSearchParams);
+  };
+
+  return (
+    <div className={styles['input-container']}>
+      <input
+        className={styles.input}
+        type="search"
+        placeholder="Search"
+        value={query}
+        onChange={onQueryChange}
+      />
+      <span className={styles['input-icon']}></span>
+    </div>
+  );
+};
